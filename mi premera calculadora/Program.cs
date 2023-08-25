83f351835536fe07cf0e4fa1f268a85764364c4e
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-08-24 23:09:42 -0500

    algoritmo con mi primera calculadora

--- mi premera calculadora/Program.cs
+++ mi premera calculadora/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//mi primera calculadora
+
+
+public class MiPrimeraCalculadora
+{
+    static void Main(string[] args)
+    {
+        //declaro vatriables de entrada
+        int num1, num2, ress, resr, resm, resd;
+
+
+        //solicito variables por pantalla
+
+        Console.Write("Ingrese el primer numero: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Ingrese el segundo numero: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+
+        //realizamos calculos
+
+        ress = num1 + num2;
+        resr = num1 - num2;
+        resm = num1 * num2;
+        resd = num1 / num2;
+
+        ////salidas
+
+        Console.Write("El resultadop de la suma es:" + ress);
+        Console.Write("\n");
+        Console.Write("El resultado de la resta es:" + resr);
+        Console.Write("\n");
+        Console.Write("El resultado de la multiplicacion es:" + resm);
+        Console.Write("\n");
+        Console.Write("El resultado de la division es:" + resd);
+
+    }
+
+
+
+
+}
+
+
+
+
